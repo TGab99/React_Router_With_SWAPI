7f506a1f9dd939855302d5f01124070559fb681e
TGab99 <55789282+TGab99@users.noreply.github.com>
2021-05-28 21:01:51 +0200

    Create Nav.js
    I created Nav.js file

---
+++ react-router/src/Nav.js
@@ -0,0 +1,32 @@
+import './Nav.css';
+import { Link } from 'react-router-dom';
+
+function App() {
+  return (
+    <nav>
+        <Link className='links-style' to='/'>Logo</Link>
+        <ul className='nav-items'>
+            <li>
+                <Link className='links-style' to="/people">People</Link>
+            </li>
+            <li>
+                <Link className='links-style' to="/planet">Planet</Link>
+            </li>
+            <li>
+                <Link className='links-style' to="/starships">Starships</Link>
+            </li>
+            <li>
+                <Link className='links-style' to="/vehicles">Vehicles</Link>
+            </li>
+            <li>
+                <Link className='links-style' to="/species">Species</Link>
+            </li>
+            <li>
+                <Link className='links-style' to="/films">Films</Link>
+            </li>
+        </ul>
+    </nav>
+  );
+}
+
+export default App;
