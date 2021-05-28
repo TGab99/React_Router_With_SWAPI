9038bdb807b464769837808924ebe13a2e538b53
TGab99 <55789282+TGab99@users.noreply.github.com>
2021-05-28 21:00:23 +0200

    Update App.js
    I remove App.css. And I imported Nav, People, PeopleName, BrowserRouter, Switch and Route. Plus I wrote the routing.

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,11 +1,19 @@
-import './App.css';
+import Nav from './Nav';
 import Home from './Home';
+import People from './People';
+import PeopleName from './PeopleName';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
-    </div>
+    <Router>
+      <Nav/>
+      <Switch>
+        <Route path='/' exact component={Home}/>
+        <Route path='/people' exact component={People}/>
+        <Route path='/people/:name' component={PeopleName}/>
+      </Switch>
+    </Router>
   );
 }
 
