61b7c904f3f70963c3f6ceda9b987a4be4db0d6c
TGab99 <55789282+TGab99@users.noreply.github.com>
2021-08-23 12:11:16 +0200

    Create VehiclesName.js
    I created VehiclesName.js file.

---
+++ react-router/src/VehiclesName.js
@@ -0,0 +1,75 @@
+import React, {useEffect, useState} from 'react';
+
+function VehiclesName( { match } ){
+
+    useEffect(() => {
+        fetchItem();
+        console.log(match);
+    }, []);
+
+    const [items, setItems] = useState([]);
+
+    const fetchItem = async () => {
+        const data = await fetch(`https://swapi.dev/api/vehicles/?search=${match.params.name}`);
+
+        const items = await data.json();
+        console.log(items.results);
+        setItems(items.results);
+    };
+    return(
+        <div>
+            {items.map((item, index) => (
+                <>
+                <h1 key={index}>{item.name}</h1>
+                <hr/>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td className='name'>Model:</td>
+                            <td className='value'>{item.model}</td>
+                        </tr>
+                        <tr>
+                            <td className='name'>Manufacturer:</td>
+                            <td className='value'>{item.manufacturer}</td>
+                        </tr>
+                        <tr>
+                            <td className='name'>Cost In Credits:</td>
+                            <td className='value'>{item.cost_in_credits}</td>
+                        </tr>
+                        <tr>
+                            <td className='name'>Length:</td>
+                            <td className='value'>{item.length}</td>
+                        </tr>
+                        <tr>
+                            <td className='name'>Max Atmosphering Speed:</td>
+                            <td className='value'>{item.max_atmosphering_speed}</td>
+                        </tr>
+                        <tr>
+                            <td className='name'>Crew:</td>
+                            <td className='value'>{item.crew}</td>
+                        </tr>
+                        <tr>
+                            <td className='name'>Passengers:</td>
+                            <td className='value'>{item.passengers}</td>
+                        </tr>
+                        <tr>
+                            <td className='name'>Cargo Capacity:</td>
+                            <td className='value'>{item.cargo_capacity}</td>
+                        </tr>
+                        <tr>
+                            <td className='name'>Consumables:</td>
+                            <td className='value'>{item.consumables}</td>
+                        </tr>
+                        <tr>
+                            <td className='name'>Vehicle Class:</td>
+                            <td className='value'>{item.vehicle_class}</td>
+                        </tr>
+                    </tbody>
+                </table>
+                </>
+            ))}
+        </div>
+    );
+}
+
+export default VehiclesName;
