c70981d90a4410a023a6126d289086d7d8c8cc1b
TGab99 <55789282+TGab99@users.noreply.github.com>
2021-08-23 11:31:10 +0200

    Create Starships.js
    I created Starships.js file

---
+++ react-router/src/Starships.js
@@ -0,0 +1,33 @@
+import React, {useEffect, useState} from 'react';
+import { Link } from 'react-router-dom';
+
+function Starships(){
+
+    useEffect(() => {
+        fetchItems();
+    }, []);
+
+    const [items, setItems] = useState([]);
+
+    const fetchItems = async () => {
+        const data = await fetch(`https://swapi.dev/api/starships/?page=1`);
+
+        const items = await data.json();
+        console.log(items.results);
+        setItems(items.results);
+    }
+
+    return(
+        <div>
+            {items.map((item, index) => (
+                <p key={index}>
+                    <Link key='name-style' to={`/starships/${item.name}`}>
+                        {item.name}
+                    </Link>
+                </p>
+            ))}
+        </div>
+    );
+}
+
+export default Starships;
