0ea8860dbd1fcc3af0fb6251ea16c054a676781f
TGab99 <55789282+TGab99@users.noreply.github.com>
2021-08-23 13:59:48 +0200

    Create SpeciesName.js
    I created SpeciesName.js file.

---
+++ react-router/src/SpeciesName.js
@@ -0,0 +1,67 @@
+import React, {useEffect, useState} from 'react';
+
+function SpeciesName({ match }) {
+
+    useEffect(() => {
+        fetchItem();
+        console.log(match);
+    }, []);
+
+    const [items, setItems] = useState([]);
+
+    const fetchItem = async () => {
+        const data = await fetch(`https://swapi.dev/api/species/?search=${match.params.name}`);
+
+        const items = await data.json();
+        console.log(items.results);
+        setItems(items.results);
+    };
+    return(
+        <div>
+            {items.map((item, index) => (
+                <>
+                <h1 key={index}>{item.name}</h1>
+                <hr/>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td className='name'>Classification:</td>
+                            <td className='value'>{item.classification}</td>
+                        </tr>
+                        <tr>
+                            <td className='name'>Designation:</td>
+                            <td className='value'>{item.designation}</td>
+                        </tr>
+                        <tr>
+                            <td className='name'>Average Height:</td>
+                            <td className='value'>{item.average_height}</td>
+                        </tr>
+                        <tr>
+                            <td className='name'>Skin Colors:</td>
+                            <td className='value'>{item.skin_colors}</td>
+                        </tr>
+                        <tr>
+                            <td className='name'>Hair Colors:</td>
+                            <td className='value'>{item.hair_colors}</td>
+                        </tr>
+                        <tr>
+                            <td className='name'>Eye Colors:</td>
+                            <td className='value'>{item.eye_colors}</td>
+                        </tr>
+                        <tr>
+                            <td className='name'>Average Lifespan:</td>
+                            <td className='value'>{item.average_lifespan}</td>
+                        </tr>
+                        <tr>
+                            <td className='name'>Language:</td>
+                            <td className='value'>{item.language}</td>
+                        </tr>
+                    </tbody>
+                </table>
+                </>
+            ))}
+        </div>
+    );
+}
+
+export default SpeciesName;
