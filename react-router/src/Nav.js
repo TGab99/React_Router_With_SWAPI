88fc8eaea34344925099efa34dd8b15717bfb6f0
TGab99 <55789282+TGab99@users.noreply.github.com>
2021-08-25 11:16:49 +0200

    Update Nav.js
    I imported App.css file and logo.svg file. I changed <Link className='links-style' to='/'>Logo</Link> to <Link className='logo-style' to='/'><img src={StarWarsLogo} alt="Star Wars Logo"/></Link>.

--- react-router/src/Nav.js
+++ react-router/src/Nav.js
@@ -1,31 +1,35 @@
-import './Nav.css';
+import './App.css';
 import { Link } from 'react-router-dom';
+import StarWarsLogo from './logo.svg';
 
 function App() {
   return (
-    <nav>
-        <Link className='links-style' to='/'>Logo</Link>
-        <ul className='nav-items'>
-            <li>
-                <Link className='links-style' to="/people">People</Link>
-            </li>
-            <li>
-                <Link className='links-style' to="/planets">Planet</Link>
-            </li>
-            <li>
-                <Link className='links-style' to="/starships">Starships</Link>
-            </li>
-            <li>
-                <Link className='links-style' to="/vehicles">Vehicles</Link>
-            </li>
-            <li>
-                <Link className='links-style' to="/species">Species</Link>
-            </li>
-            <li>
-                <Link className='links-style' to="/films">Films</Link>
-            </li>
-        </ul>
-    </nav>
+    <div className="nav-container">
+        <nav>
+            <Link className='logo-style' to='/'><img src={StarWarsLogo} alt="Star Wars Logo"/></Link>
+            <ul className='nav-items'>
+                <li>
+                    <Link className='links-style' to="/people">People</Link>
+                </li>
+                <li>
+                    <Link className='links-style' to="/planets">Planet</Link>
+                </li>
+                <li>
+                    <Link className='links-style' to="/starships">Starships</Link>
+                </li>
+                <li>
+                    <Link className='links-style' to="/vehicles">Vehicles</Link>
+                </li>
+                <li>
+                    <Link className='links-style' to="/species">Species</Link>
+                </li>
+                <li>
+                    <Link className='links-style' to="/films">Films</Link>
+                </li>
+            </ul>
+        </nav>
+        <hr/>
+    </div>
   );
 }
 
